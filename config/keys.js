8ad9e7b975074396cfe037e7a30ed3ae23a9270b
Moshe Maltz <mosheamaltz@gmail.com>
2023-07-17 13:45:40 +0300

    keys.js changed

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,6 @@ module.exports = {
         client_secret: "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",
         client_id: "600062574859-v37fdm2ida9sulsa08klkgrivh6u1495.apps.googleusercontent.com",
         redirectURI: "https://moshe-maltz-business-card-web-site.onrender.com/auth/google/redirect"
-        
     },
     mongodb : {
         DBURI:"mongodb+srv://mosheamaltz:9RKNwXy5aMazAx91@businesscardcluster.if3wd3e.mongodb.net/?retryWrites=true&w=majority" 
